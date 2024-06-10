6f5e8cd9717e8d36faa7dc375505cc45727946c7
Vitalii Shevchuk <vitshev120@gmail.com>
2024-06-10 23:59:23 +0300

    Fixed  a few things

--- Solver.py
+++ Solver.py
@@ -8,11 +8,8 @@ class Solver:
         self.input_file_name = input_file_name
         self.output_file_name = output_file_name
         self.workers = workers
-        print("Inited")
 
     def solve(self):
-        print("Job Started")
-        print("Workers %d" % len(self.workers))
 
         (n, k) = self.read_input()
         a = 1 << n
@@ -23,7 +20,6 @@ class Solver:
         # map
         mapped = []
         for i in xrange(0, len(self.workers)):
-            print("map %d" % i)
             mapped.append(self.workers[i].mymap(str(a + i * step_n), str(a + (i + 1) * step_n), step_k))
 
         # reduce
@@ -32,14 +28,10 @@ class Solver:
         # output
         self.write_output(primes)
 
-        print("Job Finished")
 
     @staticmethod
     @expose
     def mymap(a, b, count):
-        print(a)
-        print(b)
-        print(count)
         a = int(a)
         b = int(b)
         primes = []
@@ -48,7 +40,7 @@ class Solver:
             a += 1
 
         while len(primes) < count and a < b:
-            if Solver.is_probable_prime(a):
+            if Solver.is_prime(a):
                 primes.append(str(a))
             a += 2
 
@@ -75,11 +67,10 @@ class Solver:
         f.write(', '.join(output))
         f.write('\n')
         f.close()
-        print("output done")
 
     @staticmethod
     @expose
-    def is_probable_prime(n):
+    def is_prime(n):
         assert n >= 2
         if n == 2:
             return True
@@ -94,7 +85,7 @@ class Solver:
             s += 1
             d = quotient
         assert (2 ** s * d == n - 1)
-        def try_composite(a):
+        def test(a):
             if pow(a, d, n) == 1:
                 return False
             for i in range(s):
@@ -104,7 +95,7 @@ class Solver:
 
         for i in range(1):
             a = random.randrange(2, n)
-            if try_composite(a):
+            if test(a):
                 return False
 
         return True
